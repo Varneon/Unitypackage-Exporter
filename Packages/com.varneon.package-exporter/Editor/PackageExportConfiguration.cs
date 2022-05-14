c789fcc59796f3989d666454e0a42fc5a172c262
Varneon <me@varneon.com>
2022-05-14 11:37:04 -0700

    V0.1.0 initial overhaul
    -Upgraded all UIs to UIElements
    -Added package tree preview
    -Added path exclusions to packages

--- Packages/com.varneon.package-exporter/Editor/PackageExportConfiguration.cs
+++ Packages/com.varneon.package-exporter/Editor/PackageExportConfiguration.cs
@@ -1,8 +1,11 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.UIElements;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace Varneon.PackageExporter
@@ -11,49 +14,145 @@ namespace Varneon.PackageExporter
     /// Configuration definition for exporting a package
     /// </summary>
     [Serializable]
-    public class PackageExportConfiguration
+    public partial class PackageExportConfiguration
     {
         /// <summary>
         /// Name of the export configuration
         /// </summary>
-        [Tooltip("Name of the package")]
         public string Name;
 
         /// <summary>
         /// Directory where the package will be exported
         /// </summary>
-        [Tooltip("Directory where the package will be exported")]
         public string ExportDirectory = "Assets";
 
         /// <summary>
-        /// File path of the package manifest
+        /// [UPM Package Only] Package Manifest of the UPM package (package.json)
         /// </summary>
-        [Tooltip("[UPM Package Only] Path to the UPM package's manifest file (package.json)")]
-        public string ManifestFilePath;
+        public PackageManifest PackageManifest;
 
         /// <summary>
-        /// File path of the version file of the package
+        /// Version file of the package
         /// </summary>
-        [Tooltip("Path to the version.txt file of the package")]
-        public string VersionFilePath;
+        public TextAsset VersionFile;
 
         /// <summary>
-        /// Paths that will be exported
+        /// Assets that will be exported
         /// </summary>
-        [Tooltip("Paths to be included in the export")]
-        public string[] ExportedPaths;
+        public List<AssetInclusion> AssetInclusions = new List<AssetInclusion>();
 
         /// <summary>
-        /// Options for exporting the package
+        /// Folders that will be exported
         /// </summary>
-        [Tooltip("Options for exporting the package")]
-        public ExportPackageOptions ExportOptions = ExportPackageOptions.Interactive | ExportPackageOptions.Recurse;
+        public List<FolderInclusion> FolderInclusions = new List<FolderInclusion>();
+
+        /// <summary>
+        /// Assets that will be excluded from the export
+        /// </summary>
+        public List<AssetExclusion> AssetExclusions = new List<AssetExclusion>();
+
+        /// <summary>
+        /// Folders that will be excluded from the export
+        /// </summary>
+        public List<FolderExclusion> FolderExclusions = new List<FolderExclusion>();
+
+        /// <summary>
+        /// Should the package directory be opened in file browser after export
+        /// </summary>
+        public bool ShowPackageInFileBrowserAfterExport = true;
+
+        /// <summary>
+        /// Should the configuration block be expanded in the inspector
+        /// </summary>
+        [HideInInspector]
+        public bool ExpandInInspector = true;
 
         private const string LogPrefix = "[<color=#00AACC>PackageExporter</color>]:";
 
-        public PackageExportConfiguration(string[] pathsToExport)
+        public enum DependencyOptions
         {
-            ExportedPaths = pathsToExport;
+            None,
+            Direct,
+            Recursive
+        }
+
+        /// <summary>
+        /// Path inclusion definition
+        /// </summary>
+        [Serializable]
+        public class PathInclusion
+        {
+            /// <summary>
+            /// Path to an asset or folder
+            /// </summary>
+            public string Path;
+
+            /// <summary>
+            /// If the Path is a folder, should the files in the subfolders be included in the definition. Will be ignored if Path is an asset
+            /// </summary>
+            public bool IncludeSubfolders;
+
+            public PathInclusion(string path = "Assets", bool includeSubfolders = true)
+            {
+                Path = path;
+                IncludeSubfolders = includeSubfolders;
+            }
+
+            public void BindConfigurationBlockElement(VisualElement block, Action markConfigurationsDirtyAction)
+            {
+                Label foldoutLabel = block.Q<Foldout>("Foldout_Data").Q<Toggle>().Q<Label>();
+                foldoutLabel.text = Path;
+
+                TextField pathTextField = block.Q<TextField>("TextField_Path");
+                pathTextField.RegisterValueChangedCallback(a => {
+                    Path = a.newValue;
+                    foldoutLabel.text = a.newValue;
+                    markConfigurationsDirtyAction.Invoke();
+                });
+
+                pathTextField.SetValueWithoutNotify(Path);
+
+                Toggle includeSubfoldersToggle = block.Q<Toggle>("Toggle_IncludeSubfolders");
+                includeSubfoldersToggle.RegisterValueChangedCallback(a => {
+                    IncludeSubfolders = a.newValue;
+                    markConfigurationsDirtyAction.Invoke();
+                });
+
+                includeSubfoldersToggle.SetValueWithoutNotify(IncludeSubfolders);
+            }
+        }
+
+        /// <summary>
+        /// Path exclusion definition
+        /// </summary>
+        [Serializable]
+        public class PathExclusion
+        {
+            /// <summary>
+            /// Path to the asset or folder
+            /// </summary>
+            public string Path;
+
+            public PathExclusion(string path = "Assets")
+            {
+                Path = path;
+            }
+
+            public void BindConfigurationBlockElement(VisualElement block, Action markConfigurationsDirtyAction)
+            {
+                TextField pathTextField = block.Q<TextField>("TextField_Path");
+                pathTextField.RegisterValueChangedCallback(a => {
+                    Path = a.newValue;
+                    markConfigurationsDirtyAction.Invoke();
+                });
+
+                pathTextField.SetValueWithoutNotify(Path);
+            }
+        }
+
+        public PackageExportConfiguration(string name)
+        {
+            Name = name;
         }
 
         /// <summary>
@@ -94,16 +193,9 @@ namespace Varneon.PackageExporter
         /// <returns>File path to the package manifest if it's valid</returns>
         public string GetManifestPath()
         {
-            if (!string.IsNullOrEmpty(ManifestFilePath))
+            if(PackageManifest != null)
             {
-                if (File.Exists(ManifestFilePath))
-                {
-                    return ManifestFilePath;
-                }
-                else
-                {
-                    Debug.LogWarning($"{LogPrefix} Manifest file can't be found in this project in configuration: (<color=red>{Name}</color>)", PackageExporterConfigurations.Load());
-                }
+                return AssetDatabase.GetAssetPath(PackageManifest);
             }
 
             return string.Empty;
@@ -115,16 +207,9 @@ namespace Varneon.PackageExporter
         /// <returns>File path to the version file if it's valid</returns>
         public string GetVersionPath()
         {
-            if (!string.IsNullOrEmpty(VersionFilePath))
+            if(VersionFile != null)
             {
-                if (File.Exists(VersionFilePath))
-                {
-                    return VersionFilePath;
-                }
-                else
-                {
-                    Debug.LogWarning($"{LogPrefix} Version file can't be found in this project in configuration: (<color=red>{Name}</color>)", PackageExporterConfigurations.Load());
-                }
+                return AssetDatabase.GetAssetPath(VersionFile);
             }
 
             return string.Empty;
---
+++ Packages/com.varneon.package-exporter/Editor/PackageExportConfigurationInclusions.cs
@@ -0,0 +1,160 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+using Object = UnityEngine.Object;
+
+namespace Varneon.PackageExporter
+{
+    /// <summary>
+    /// Inclusion classes for configurations
+    /// </summary>
+    public partial class PackageExportConfiguration
+    {
+        public interface IInclusion
+        {
+            List<string> GetPaths();
+
+            void BindConfigurationBlockElement(VisualElement block, Action markConfigurationsDirtyAction);
+        }
+
+        [Serializable]
+        public class AssetInclusion : IInclusion
+        {
+            public Object Asset;
+
+            public DependencyOptions DependencyOptions;
+
+            public bool Expanded = true;
+
+            public string PathPreview
+            {
+                get
+                {
+                    return $"Asset: {AssetDatabase.GetAssetPath(Asset)}";
+                }
+            }
+
+            public List<string> GetPaths()
+            {
+                List<string> paths = new List<string>();
+
+                if (Asset == null) { return paths; }
+
+                string assetPath = AssetDatabase.GetAssetPath(Asset);
+
+                paths.Add(assetPath);
+
+                if (DependencyOptions != DependencyOptions.None)
+                {
+                    string[] dependencies = AssetDatabase.GetDependencies(assetPath, DependencyOptions == DependencyOptions.Recursive);
+
+                    paths.AddRange(dependencies);
+                }
+
+                return paths;
+            }
+
+            public void BindConfigurationBlockElement(VisualElement block, Action markConfigurationsDirtyAction)
+            {
+                Foldout foldout = block.Q<Foldout>("Foldout_Data");
+                foldout.value = Expanded;
+                foldout.RegisterValueChangedCallback(a => Expanded = a.newValue);
+
+                Label foldoutLabel = foldout.Q<Toggle>().Q<Label>();
+                foldoutLabel.text = PathPreview;
+
+                ObjectField assetField = block.Q<ObjectField>("ObjectField_Asset");
+                assetField.objectType = typeof(Object);
+                assetField.RegisterValueChangedCallback(a => {
+                    Asset = a.newValue;
+                    foldoutLabel.text = PathPreview;
+                });
+                assetField.SetValueWithoutNotify(Asset);
+
+                EnumField dependencyOptionsField = block.Q<EnumField>("EnumField_DependencyOptions");
+                dependencyOptionsField.Init(DependencyOptions);
+                dependencyOptionsField.RegisterValueChangedCallback(a => {
+                    DependencyOptions = (DependencyOptions)a.newValue;
+                });
+            }
+        }
+
+        [Serializable]
+        public class FolderInclusion : IInclusion
+        {
+            public DefaultAsset Folder;
+
+            public bool IncludeSubfolders = true;
+
+            public DependencyOptions DependencyOptions;
+
+            public bool Expanded = true;
+
+            public string PathPreview
+            {
+                get
+                {
+                    return $"Folder: {AssetDatabase.GetAssetPath(Folder)}";
+                }
+            }
+
+            public List<string> GetPaths()
+            {
+                List<string> paths = new List<string>();
+
+                if (Folder == null) { return paths; }
+
+                string folderPath = AssetDatabase.GetAssetPath(Folder);
+
+                string[] files = (IncludeSubfolders ? Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories) : Directory.GetFiles(folderPath)).Where(d => !d.EndsWith(".meta")).Select(c => c.Replace('\\', '/')).ToArray();
+
+                paths.AddRange(files);
+
+                if (DependencyOptions != DependencyOptions.None)
+                {
+                    string[] dependencies = AssetDatabase.GetDependencies(files, DependencyOptions == DependencyOptions.Recursive);
+
+                    paths.AddRange(dependencies);
+                }
+
+                return paths;
+            }
+
+            public void BindConfigurationBlockElement(VisualElement block, Action markConfigurationsDirtyAction)
+            {
+                Foldout foldout = block.Q<Foldout>("Foldout_Data");
+                foldout.value = Expanded;
+                foldout.RegisterValueChangedCallback(a => Expanded = a.newValue);
+
+                Label foldoutLabel = foldout.Q<Toggle>().Q<Label>();
+                foldoutLabel.text = PathPreview;
+
+                ObjectField folderField = block.Q<ObjectField>("ObjectField_Folder");
+                folderField.objectType = typeof(Object);
+                folderField.RegisterValueChangedCallback(a => {
+                    Folder = a.newValue as DefaultAsset;
+                    if (Folder == null) { folderField.SetValueWithoutNotify(null); }
+                    foldoutLabel.text = PathPreview;
+                });
+                folderField.SetValueWithoutNotify(Folder);
+
+                Toggle subfoldersToggle = block.Q<Toggle>("Toggle_Subfolders");
+                subfoldersToggle.RegisterValueChangedCallback(a => {
+                    IncludeSubfolders = a.newValue;
+                });
+                subfoldersToggle.SetValueWithoutNotify(IncludeSubfolders);
+
+                EnumField dependencyOptionsField = block.Q<EnumField>("EnumField_DependencyOptions");
+                dependencyOptionsField.Init(DependencyOptions);
+                dependencyOptionsField.RegisterValueChangedCallback(a => {
+                    DependencyOptions = (DependencyOptions)a.newValue;
+                });
+            }
+        }
+    }
+}
